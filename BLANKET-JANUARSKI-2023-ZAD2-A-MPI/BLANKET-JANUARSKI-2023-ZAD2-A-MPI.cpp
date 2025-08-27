d826e6c40ac1853b1453ea9b97b02f517089c6ef
Djole <djordje.glisic@elfak.rs>
2025-08-27 15:35:18 +0200

    Djole

--- BLANKET-JANUARSKI-2023-ZAD2-A-MPI/BLANKET-JANUARSKI-2023-ZAD2-A-MPI.cpp
+++ BLANKET-JANUARSKI-2023-ZAD2-A-MPI/BLANKET-JANUARSKI-2023-ZAD2-A-MPI.cpp
@@ -54,10 +54,10 @@ int main(int argc, char* argv[])
 	MPI_Type_vector(K, N, 0, MPI_INT, &vrste);
 	MPI_Type_commit(&vrste);
 	MPI_Datatype kolone;
-	MPI_Type_vector(1, K, 0, MPI_INT, &kolone);
+	MPI_Type_vector(N, K, N, MPI_INT, &kolone);
 	MPI_Type_commit(&kolone);
 	int* parcA = (int*)malloc(K * N * sizeof(int));
-	int* parcB = (int*)malloc(K * sizeof(int));
+	int* parcB = (int*)malloc(N*K * sizeof(int));
 	int t = size;
 	int kolko = N / K;
 	if (rank == 0)
@@ -65,7 +65,21 @@ int main(int argc, char* argv[])
 		for (int i = 0;i < K;i++)
 		{
 			for (int j = 0;j < N;j++)
+			{
 				parcA[i * N + j] = A[i * N + j];
+				//printf("Proces%d a[%d][%d]=%d\n",rank,i,j, parcA[i * N + j]);
+			
+			}
+		}
+		for (int i = 0;i < N;i++)
+		{
+			for (int j = 0;j < K;j++)
+			{
+
+				parcB[i * N + j] = B[i * N + j];
+				printf("Proces%d b[%d][%d]=%d\n", rank, i, j, parcB[i * N + j]);
+
+			}
 		}
 
 		for (int i = 1;i < size;i++)
@@ -80,7 +94,7 @@ int main(int argc, char* argv[])
 	else
 	{
 		MPI_Recv(parcA, K * N, MPI_INT, 0, rank, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
-		MPI_Recv(parcB, K, MPI_INT, 0, t*rank, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+		MPI_Recv(parcB, N*K, MPI_INT, 0, t*rank, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
 
 	}
 	int* parcC = (int*)malloc(K * K * sizeof(int));
@@ -92,20 +106,70 @@ int main(int argc, char* argv[])
 	loc.val = -1;
 	for (int i = 0;i < K;i++)
 	{
-		parcC[i] = 0;
 		for (int j = 0;j < K;j++)
 		{
+			parcC[i*K+j] = 0;
 			for (int z = 0;z < N;z++)
 			{
-				parcC[i] += parcA[j * N + z] * parcB[j * K + z];
+				if (rank == 0)
+				{
+					printf("B[%d][%d]=%d\n", z, j, parcB[z * K + j]);
+
+				}
+				parcC[i*K+j] += parcA[i * N + z] * parcB[z*K+j];
 			}
+			if (parcC[i*K+j] > loc.val)
+				loc.val = (double)parcC[i*K+j];
+	
 		}
-		if (parcC[i] > loc.val)
-			loc.val = parcC[i];
+	}
+	if (rank == 0)
+	{
+		printf("MAtrica Cprac\n");
+		for (int i = 0;i < K;i++)
+		{
+			printf("[");
+			for (int j = 0;j < K;j++)
+				printf(" %d ", parcC[i * K + j]);
+			printf("]\n");
+		}
 	}
 	MPI_Reduce(&loc, &glob, 1, MPI_DOUBLE_INT, MPI_MAXLOC, 0, MPI_COMM_WORLD);
-	int q = N / K;
-	int*C=(int*)malloc(K*K)
+	MPI_Comm COL;
+	int colorK;
+	colorK = rank % kolko;
+	MPI_Comm_split(MPI_COMM_WORLD, colorK,0, &COL);
+	int newRank;
+	MPI_Comm_rank(COL, &newRank);
+	int* Ccol = nullptr; 
+	if (newRank == 0)
+	{
+		Ccol=(int*)malloc(kolko* K* K * sizeof(int));
+		
+	}
+	MPI_Gather(parcC, K* K, MPI_INT, Ccol, K* K, MPI_INT, 0, COL);
+	if(rank==0)
+		C=(int*)malloc(N* N * sizeof(int));
+	MPI_Comm VRS;
+	int colorV;
+	colorV = rank / kolko;
+	MPI_Comm_split(MPI_COMM_WORLD, colorV, 0, &VRS);
+	if (colorV == 0)
+	{
+		MPI_Gather(Ccol, kolko* K* K, MPI_INT, C, kolko* K* K, MPI_INT, 0, VRS);
+	}
+	//if (rank == 0)
+	//{
+	//	printf("Proces%d je imao najveci element matrice A i to je %lf a rezultujca matrica\n", glob.rank, glob.val);
+	//	for (int i = 0;i < N;i++)
+	//	{
+	//		printf("[");
+	//		for (int j = 0;j < N;j++)
+	//			printf(" %d ", C[i * N + j]);
+	//		printf("]\n");
+	//	}
+	//}
+	
 	MPI_Finalize();
 	return 0;
 }
